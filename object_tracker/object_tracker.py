d3dc96e9e1b31a8d8a1c521434060699f2e15534
Niels Verleysen <nlsverleysen99@gmail.com>
2022-11-23 19:17:40 +0100

    Add basic object tracking based on color

--- object_tracker/object_tracker.py
+++ object_tracker/object_tracker.py
@@ -1,6 +1,9 @@
 import cv2
 import logging
 
+MIN_COLOR = (245, 182, 66)
+MAX_COLOR = (219, 252, 3)
+
 if __name__ == "__main__":
      # Logging config
     logging.basicConfig(level=logging.INFO)
@@ -26,6 +29,37 @@ if __name__ == "__main__":
             logging.error("Unable to capture the next image")
             break 
         
+        # Apply gaussian blurring
+        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        # Transform to HSV color space
+        hsv_frame = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+        
+        # Create a color mask
+        masked = cv2.inRange(hsv_frame, MIN_COLOR, MAX_COLOR)
+        # Remove small blobs (false detections)
+        masked = cv2.erode(masked, None, iterations=2) 
+        masked = cv2.dilate(masked, None, iterations=2)
+        
+        # Similar to the document scanner, find contours in the mask
+        contours, _ = cv2.findContours(masked.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+        # Let's find our tennisball
+        center = None
+        if len(contours) > 0:
+            # Select the contour with the largest area
+            selected_contour = max(contours, key=cv2.contourArea)
+            
+            # Determine a circle that encloses the selected contour
+            ((x, y), radius) = cv2.minEnclosingCircle(selected_contour)
+            
+            # Determine the center of the ball
+            moments = cv2.moments(selected_contour)
+            center = (int(moments["m10"] / moments["m00"]), int(moments["m01"] / moments["m00"]))
+            
+            # If the radius is large enough we can draw the circle on the frame
+            if radius > 10:
+                cv2.circle(frame, (int(x), int(y)), int(radius), (255, 0, 0), 2)
+
         # Display the frame
         cv2.imshow("frame", frame)
         
